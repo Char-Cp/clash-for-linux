0c0e8723931a745a9df44090e53cb75c0fbd5a5a
chenp <chenpeng@copote.com>
2024-09-18 09:25:51 +0800

    更新节点

---
+++ update.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# 加载系统函数库(Only for RHEL Linux)
+# [ -f /etc/init.d/functions ] && source /etc/init.d/functions
+
+#################### 脚本初始化任务 ####################
+
+# 获取脚本工作目录绝对路径
+export Server_Dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+# 加载.env变量文件
+source $Server_Dir/.env
+
+# 给二进制启动程序、脚本等添加可执行权限
+chmod +x $Server_Dir/bin/*
+chmod +x $Server_Dir/scripts/*
+chmod +x $Server_Dir/tools/subconverter/subconverter
+
+
+
+#################### 变量设置 ####################
+
+Conf_Dir="$Server_Dir/conf"
+Temp_Dir="$Server_Dir/temp"
+Log_Dir="$Server_Dir/logs"
+
+# 将 CLASH_URL 变量的值赋给 URL 变量，并检查 CLASH_URL 是否为空
+URL=${CLASH_URL:?Error: CLASH_URL variable is not set or empty}
+
+
+
+#################### 函数定义 ####################
+
+# 自定义action函数，实现通用action功能
+success() {
+	echo -en "\\033[60G[\\033[1;32m  OK  \\033[0;39m]\r"
+	return 0
+}
+
+failure() {
+	local rc=$?
+	echo -en "\\033[60G[\\033[1;31mFAILED\\033[0;39m]\r"
+	[ -x /bin/plymouth ] && /bin/plymouth --details
+	return $rc
+}
+
+action() {
+	local STRING rc
+
+	STRING=$1
+	echo -n "$STRING "
+	shift
+	"$@" && success $"$STRING" || failure $"$STRING"
+	rc=$?
+	echo
+	return $rc
+}
+
+# 判断命令是否正常执行 函数
+if_success() {
+	local ReturnStatus=$3
+	if [ $ReturnStatus -eq 0 ]; then
+		action "$1" /bin/true
+	else
+		action "$2" /bin/false
+		exit 1
+	fi
+}
+#################### 任务执行 ####################
+
+## 获取CPU架构信息
+# Source the script to get CPU architecture
+source $Server_Dir/scripts/get_cpu_arch.sh
+
+# Check if we obtained CPU architecture
+if [[ -z "$CpuArch" ]]; then
+	echo "Failed to obtain CPU architecture"
+	exit 1
+fi
+
+
+
+
+# 拉取更新config.yml文件
+echo -e '\n正在下载Clash配置文件...'
+Text3="配置文件config.yaml下载成功！"
+Text4="配置文件config.yaml下载失败，退出启动！"
+
+# 尝试使用curl进行下载
+curl -L -k -sS --retry 5 -m 10 -o $Temp_Dir/clash.yaml $URL
+ReturnStatus=$?
+if [ $ReturnStatus -ne 0 ]; then
+	# 如果使用curl下载失败，尝试使用wget进行下载
+	for i in {1..10}
+	do
+		wget -q --no-check-certificate -O $Temp_Dir/clash.yaml $URL
+		ReturnStatus=$?
+		if [ $ReturnStatus -eq 0 ]; then
+			break
+		else
+			continue
+		fi
+	done
+fi
+if_success $Text3 $Text4 $ReturnStatus
+
+# 重命名clash配置文件
+\cp -a $Temp_Dir/clash.yaml $Temp_Dir/clash_config.yaml
+
+
+## 判断订阅内容是否符合clash配置文件标准，尝试转换（当前不支持对 x86_64 以外的CPU架构服务器进行clash配置文件检测和转换，此功能将在后续添加）
+if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64"  ]]; then
+	echo -e '\n判断订阅内容是否符合clash配置文件标准:'
+	bash $Server_Dir/scripts/clash_profile_conversion.sh
+	sleep 3
+fi
+
+
+## Clash 配置文件重新格式化及配置
+# 取出代理相关配置
+#sed -n '/^proxies:/,$p' $Temp_Dir/clash.yaml > $Temp_Dir/proxy.txt
+sed -n '/^proxies:/,$p' $Temp_Dir/clash_config.yaml > $Temp_Dir/proxy.txt
+
+# 合并形成新的config.yaml
+cat $Temp_Dir/templete_config.yaml > $Temp_Dir/config.yaml
+cat $Temp_Dir/proxy.txt >> $Temp_Dir/config.yaml
+\cp $Temp_Dir/config.yaml $Conf_Dir/
+
+# Configure Clash Dashboard
+Work_Dir=$(cd $(dirname $0); pwd)
+Dashboard_Dir="${Work_Dir}/dashboard/public"
+sed -ri "s@^# external-ui:.*@external-ui: ${Dashboard_Dir}@g" $Conf_Dir/config.yaml
+sed -r -i '/^secret: /s@(secret: ).*@\1'${Secret}'@g' $Conf_Dir/config.yaml
